72ed4754ec2485fc59c2cf9f9c4408cef6c02638
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-08-29 19:12:56 +0600

    search btn add

--- food.js
+++ food.js
@@ -12,9 +12,9 @@ const displayMeal = (foods) => {
     const div = document.createElement("div");
     div.innerHTML = `
     <div class="card h-100">
-        <img src="" class="card-img-top" alt="...">
+        <img src="${food.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">${food.strMeal}</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         </div>
     </div>
